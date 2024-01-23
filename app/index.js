7b438fc5bb910d9f00b27d3a9275f11b710e931e
Filippo <filipporosas@192.168.1.121>
2024-01-23 16:51:25 +0000

    add unistyles

--- app/index.js
+++ app/index.js
@@ -1,20 +1,27 @@
+import 'react-native-unistyles';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import {  createStyleSheet, useStyles } from 'react-native-unistyles'
 
 export default function App() {
+  const { styles } = useStyles(stylesheet)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const stylesheet = createStyleSheet(() => ({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontSize: 22,
+        color: 'red',
+    }
+}))
